bff341c905959ade9c1c2ec70fcfd037aac42a70
Pavan Sabbineni <pavan2884@gmail.com>
2022-02-25 15:50:48 -0800

    Add log in honeypot api

--- src/pages/api/honeypot.ts
+++ src/pages/api/honeypot.ts
@@ -3,6 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { honeypot } from "../../config";
 
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
+  console.log("Honeypot", process.env.HONEYPOT_WALLET_ADDRESS);
   if (process.env.HONEYPOT_WALLET_ADDRESS) {
     res.status(200).json(honeypot);
   } else {
